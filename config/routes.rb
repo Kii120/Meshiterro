a67a88246eac9204cb5d000e4b2a46963aa1821f
EC2 Default User <ec2-user@ip-172-31-4-33.ec2.internal>
2022-12-28 01:07:24 +0000

    [update] until 17sec

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,17 @@ Rails.application.routes.draw do
   # get 'post_images/new'
   # get 'post_images/index'
   # get 'post_images/show'
-
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  # onlyを使うことで，resourcesが作ってくれるアクションを指定できる．
-  resources :users, only: [:show, :edit, :update]
   
+  # これ一番
   devise_for :users
   # get 'homes/top'
   root to: "homes#top"
   
   get "homes/about", as: 'about'
+  
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  # onlyを使うことで，resourcesが作ってくれるアクションを指定できる．
+  resources :users, only: [:show, :edit, :update]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
